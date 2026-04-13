a49d6b0c1d6e09aac53be7900234af4ca90e71c3
Anil <anildukkipatty@gmail.com>
2026-04-13 09:34:35 +0530

    changed input wrt web from ios

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -8,7 +8,6 @@ import { saveVmUrl } from "@/store/url-store";
 import {
   BottomSheetBackdrop,
   BottomSheetModal,
-  BottomSheetTextInput,
   BottomSheetView,
 } from "@gorhom/bottom-sheet";
 import { Image } from "expo-image";
@@ -29,6 +28,7 @@ import {
   SafeAreaView,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   View,
 } from "react-native";
@@ -510,7 +510,7 @@ function AuthSheet({
               /> */}
 
               <EmailPlaceHolderIcon style={styles.inputIconImage} />
-              <BottomSheetTextInput
+              <TextInput
                 style={[styles.input, !email && styles.inputPlaceholder]}
                 placeholder="name@email.com"
                 placeholderTextColor="#59B26E"
@@ -528,7 +528,7 @@ function AuthSheet({
           <>
             <Text style={styles.fieldLabel}>OTP CODE</Text>
             <View style={styles.inputRow}>
-              <BottomSheetTextInput
+              <TextInput
                 style={[styles.input, !otp && styles.inputPlaceholder]}
                 placeholder="Enter 6-digit code"
                 placeholderTextColor="#59B26E"
