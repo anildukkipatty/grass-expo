1679237db46696c2d929a8384be9d2e9fc8b51c7
Anil <anildukkipatty@gmail.com>
2026-04-14 01:20:29 +0530

    base url changed to prod

--- api/client.ts
+++ api/client.ts
@@ -1,6 +1,6 @@
 // const BASE_URL = "https://uat.revise.network/grass/api";
-const BASE_URL = "http://100.70.11.43:4008/api";
-// const BASE_URL = "https://api.codeongrass.com/v1"
+// const BASE_URL = "http://100.70.11.43:4008/api";
+const BASE_URL = "https://api.codeongrass.com/v1"
 
 type RequestOptions = {
   method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
